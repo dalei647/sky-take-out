c83cd1030c46f5ee3b63aa065b4e6dc7509a14c2
dalei647 <alei258000@gmail.com>
2026-04-19 17:03:31 +0800

    C端添加购物车发

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,7 +1,35 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.ShoppingCart;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 @Mapper
 public interface ShoppingCartMapper {
+
+    /**
+     * 添加购物车
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, number, amount, create_time) " +
+            "VALUES (#{name}, #{image}, #{userId}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{createTime})")
+    void add(ShoppingCart shoppingCart);
+
+    /**
+     * 查询购物车数据
+     * @return
+     */
+    List<ShoppingCart> select(ShoppingCart shoppingCart);
+
+    /**
+     * 更新购物车数据
+     * @param item
+     */
+    @Update("update shopping_cart set number = #{number} where name = #{name}")
+    void update(ShoppingCart item);
 }
--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -1,24 +1,73 @@
 package com.sky.service.impl;
 
+import com.sky.context.BaseContext;
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
 import com.sky.entity.ShoppingCart;
+import com.sky.interceptor.JwtTokenUserInterceptor;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
 import com.sky.mapper.ShoppingCartMapper;
+import com.sky.properties.JwtProperties;
 import com.sky.service.ShoppingCartService;
+import com.sky.utils.JwtUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Autowired
     private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private SetmealMapper setmealMapper;
 
     /**
      * 添加购物车
      * @param shoppingCartDTO
      */
+    @Transactional
     public void add(ShoppingCartDTO shoppingCartDTO) {
         ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+        // 判断当前购物车是否已经存在
+        List<ShoppingCart> list = shoppingCartMapper.select(shoppingCart);
+        if(list != null && list.size() > 0){
+            // 已经存在，数量加1
+            list.forEach(item -> {
+                item.setNumber(item.getNumber() + 1);
+                // 更新
+                shoppingCartMapper.update(item);
+            });
+        }else{
+            // 不存在，创建购物车
+            if(shoppingCart.getDishId() != null){
+                // 如果添加菜品，查询菜品信息并拷贝到购物车
+                Dish dish = dishMapper.getById(shoppingCart.getDishId());
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+            }else{
+                // 如果添加套餐，查询套餐信息并拷贝到购物车
+                Setmeal setmeal = setmealMapper.getById(shoppingCart.getSetmealId());
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            // 添加购物车
+            shoppingCartMapper.add(shoppingCart);
+        }
+
     }
 }
